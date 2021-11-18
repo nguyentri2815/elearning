7bf297d39522eb78a9d7eb2417d2e88a3edc9737
nguyễn văn trí <nguyentri2815@gmail.com>
2021-11-19 03:16:19 +0700

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
